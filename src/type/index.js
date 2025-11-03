9b99f70c6766df16ce8fc70d617171f06be2c6d5
Masaya Yamamoto <masaya1122baseball@gmail.com>
2025-11-03 21:31:15 +0900

    型の学習-20251103

---
+++ src/type/index.js
@@ -0,0 +1,35 @@
+// boolean型
+var hasValue = true;
+// number型（整数、浮動小数点数）
+var count = 10;
+var float = 3.14;
+var negative = -10;
+// string型（単一引用符、ダブル引用符、バッククオート）
+var single = 'Hello';
+var double = "hello";
+var back = "hello";
+// 型推論ができない、効かない場合に型注釈を使用する
+// オブジェクト型
+var person = {
+    name: 'jack',
+    age: 20,
+};
+console.log(person.name);
+// 配列型
+var fruits = ['apple', 'banana', 'cherry'];
+// タプル型
+var book = ['book', 10, true];
+// 列挙型(特定のまとまったグループのみを指定できる)
+var CoffeeSize;
+(function (CoffeeSize) {
+    CoffeeSize[CoffeeSize["SHORT"] = 0] = "SHORT";
+    CoffeeSize[CoffeeSize["TALL"] = 1] = "TALL";
+    CoffeeSize[CoffeeSize["GRANDE"] = 2] = "GRANDE";
+    CoffeeSize[CoffeeSize["VENTI"] = 3] = "VENTI";
+})(CoffeeSize || (CoffeeSize = {}));
+var coffee = {
+    hot: true,
+    size: CoffeeSize.SHORT,
+};
+coffee.size = CoffeeSize.TALL;
+console.log(coffee.size);
--- src/type/index.ts
+++ src/type/index.ts
@@ -1,12 +1,50 @@
 // boolean型
-let hasValue: boolean = true;
+let hasValue = true;
 
 // number型（整数、浮動小数点数）
-let count: number = 10;
-let float: number = 3.14;
-let negative: number = -10;
+let count = 10;
+let float = 3.14;
+let negative = -10;
 
 // string型（単一引用符、ダブル引用符、バッククオート）
-let single: string = 'Hello';
-let double: string = "hello";
-let back: string = `hello`;
+let single = 'Hello';
+let double = "hello";
+let back = `hello`;
+
+// 型推論ができない、効かない場合に型注釈を使用する
+
+// オブジェクト型
+const person: { name: string, age: number} = {
+  name: 'jack',
+  age: 20,
+}
+
+console.log(person.name);
+
+// 配列型
+const fruits: string[] = ['apple', 'banana', 'cherry'];
+
+// タプル型
+const book: [string, number, boolean] = ['book', 10, true];
+
+// 列挙型(特定のまとまったグループのみを指定できる)
+enum CoffeeSize {
+  SHORT,
+  TALL,
+  GRANDE,
+  VENTI,
+}
+
+const coffee = {
+  hot: true,
+  size: CoffeeSize.SHORT,
+}
+
+coffee.size = CoffeeSize.TALL;
+console.log(coffee.size);
+
+// any型
+let anything: any = 'Hello';
+anything = 10;
+anything = {};
+anything.fijiti = 'fijiti';
